1631b910b669d5b7e8695fc2aaefa6181ed50810
Manish4586 <manish.n.manish45@gmail.com>
2019-11-21 00:36:09 +0530

    smartHome: implement WiFi driver for google assistant
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- smartHome.ino
+++ smartHome.ino
@@ -7,68 +7,52 @@
 */
 
 // Start Pin Layout
+#define BLYNK_PRINT Serial
+#define EspSerial Serial1
 #include <SoftwareSerial.h>
-SoftwareSerial BT(10, 11);
-String command;
+#include<SPI.h>
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+#define ESP8266_BAUD 19200
+ESP8266 wifi(&EspSerial);
 // End Pin Layout
 
-void setup(void)
+// Start Wifi Properties
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+// End Wifi Properties
 
+// ESP8266 Properties
+SoftwareSerial EspSerial(2, 3);
+String s;
+
+BLYNK_WRITE(V0)
 {
-// Serial BPS debugging information via the Serial monitor
-  BT.begin(19200);
-  Serial.begin(19200);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  s=param.asStr();
+  Serial.print(s);
+  if(s=="on")
+ {
+  digitalWrite(7, LOW);
+ }
+  else if(s=="off")
+ {
+  digitalWrite(7, HIGH);
+ }
 }
 
-void loop() {
-  while (BT.available())
-  {
+void setup(void)
+{
+// Serial BPS debugging information via the Serial monitor
+  Serial.begin(19200);
+  pinMode(7,OUTPUT);
+  EspSerial.begin(ESP8266_BAUD);
   delay(10);
-  char c = BT.read();
-  command += c;
-  } 
-  if (command.length() > 0)
-  {
-    Serial.println(command);
-  if(command == "light on")
-  {
-    digitalWrite(2, HIGH);
-  }
-  else if(command == "light off")
-  {
-    digitalWrite(2, LOW);
-  }
-  else if (command == "lamp on")
-  {
-    digitalWrite (3, HIGH);
-  }
-  else if ( command == "lamp off")
- {
-   digitalWrite (3, LOW);
- }
-  else if (command == "fan on")
- {
-   digitalWrite (4, HIGH);
- }
-  else if (command == "fan off")
- {
-   digitalWrite (4, LOW);
- }
-  else if (command == "all on")
- {
-   digitalWrite (2, HIGH);
-   digitalWrite (3, HIGH);
-   digitalWrite (4, HIGH);
- }
-  else if (command == "off")
- {
-   digitalWrite (2, LOW);
-   digitalWrite (3, LOW);
-   digitalWrite (4, LOW);
- }
-command="";
- }
+ 
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void loop(void)
+{
+  Blynk.run();
 }
