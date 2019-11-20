5472084c371da023869b0c423ebc6230a6a3d574
Manish4586 <manish.n.manish45@gmail.com>
2019-11-20 19:41:37 +0530

    gas: implement forcesensor (wip)
    Signed-off-by: Manish4586 <Manish4586@codeaurora.org>

--- gas_sensor.ino
+++ gas_sensor.ino
@@ -7,7 +7,7 @@
 */
 
 // Start Pin Layout
-int MQPin0 = 1;
+int MQPin = 1;
 int solenoidPin = 7;
 int sensorThres = 900;
 int LDRSensorPin = 3;
@@ -15,68 +15,60 @@ int forcePin = 2;
 #define out 8
 int LDRDetected;
 int forceReading;
+int solenoidThres;
 // End Pin Layout
 
-void setup() {
+void setup(void) {
 // Serial BPS debugging information via the Serial monitor
   Serial.begin(19200);
   pinMode(solenoidPin, OUTPUT);
-  pinMode(MQPin0, INPUT);
+  pinMode(MQPin, INPUT);
   pinMode(out, OUTPUT);
   pinMode(LDRSensorPin, INPUT);
   pinMode(forcePin, INPUT);
 }
 
-void loop() {
-  int analogSensor = analogRead(MQPin0);
+void loop(void) {
+  int analogSensor = analogRead(MQPin);
   LDRDetected = digitalRead(LDRSensorPin);
-  digitalWrite(out, HIGH);
-  digitalWrite(out1, LOW);
+// digitalWrite(out, HIGH);
+// digitalWrite(out1, LOW);
   Serial.print("Default LDR reading = ");
   Serial.println(analogSensor);
+{
   forceReading = analogRead(forcePin);
-  Serial.print("Default Pressure reading = ");
-  Serial.println(forceReading);  
-
+  Serial.print("Default Load reading = ");
+  Serial.println(forceReading);
+  solenoidThres = map(forceReading, 0, 1023, 0, 255);
+  analogWrite(solenoidPin, solenoidThres);
+  delay(0);  
+}
 // Online Ports
-//  Serial.print("Online Ports: comA0, comA1, comA3, comD4, comD10, comD11, ");
+//  Serial.print("Online Ports: comA0, comA1, comA2, comA3, comD4, comD10, comD11, ");
 
   if (analogSensor > sensorThres)
   {  
   digitalWrite(solenoidPin, LOW);
-  delay(analogSensor); //Wait 0 MS(debbuging)
+  delay(analogSensor);
   }
   else
   {
-  Serial.print("Smoke Detected, ");
+  Serial.print("Gas Detected, ");
   digitalWrite(solenoidPin, HIGH);
-  delay(sensorThres); //Wait 1 MS
+  delay(sensorThres);
  }
 
   if (LDRDetected < 100)
    {
   digitalWrite(solenoidPin, LOW);
- /* 
-  delay(0); //Wait 0 MS(debbuging)
- */
+  
+// delay(0); //Wait 0 MS(debbuging)
   }
   else
   if (LDRDetected > 100)
   { 
   digitalWrite(solenoidPin, HIGH);
   delay(LDRDetected);
-  delay(0);//Wait 0 MS
-  }
-
- if (forceReading == 0)
-   {
-  digitalWrite(solenoidPin, HIGH);
-  delay(0); //Wait 0 MS(debbuging)
-  }
-  else
-  if (forceReading == 1023)
-  { 
-  digitalWrite(solenoidPin, LOW);
-  delay(0);
+  delay(0); //Wait 0 MS
   }
 }
