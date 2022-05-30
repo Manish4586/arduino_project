dbd283b699b3b8fcc0f1fc7c44aaf876db6949aa
Manish Sarkar <Manish.n.manish45@gmail.com>
2022-05-30 18:48:30 +0530

    HA: initial public release
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- smartHome.ino
+++ smartHome.ino
@@ -4,55 +4,165 @@
  *  Telegram Contact : https://t.me/Manish4586
  *  Author : Manish4586 <manish.n.manish45@gmail.com>
  *  Status : Debugging
+ *  SinricPro 2.9.14 Library:     https://github.com/sinricpro/esp8266-esp32-sdk
 */
 
 // Start Pin Layout
-#define BLYNK_PRINT Serial
-#define EspSerial Serial1
-#include <SoftwareSerial.h>
-#include<SPI.h>
-#include <ESP8266_Lib.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#define ESP8266_BAUD 19200
-ESP8266 wifi(&EspSerial);
-// End Pin Layout
+//#define ENABLE_DEBUG
 
-// Start Wifi Properties
-char auth[] = "";
-char ssid[] = "";
-char pass[] = "";
-// End Wifi Properties
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
 
-// ESP8266 Properties
-SoftwareSerial EspSerial(2, 3);
-String s;
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
 
-BLYNK_WRITE(V0)
-{
-  s=param.asStr();
-  Serial.print(s);
-  if(s=="on")
- {
-  digitalWrite(7, LOW);
- }
-  else if(s=="off")
- {
-  digitalWrite(7, HIGH);
- }
+#define WIFI_SSID         "Airtel"    
+#define WIFI_PASS         "123456789"
+#define APP_KEY           "APP_KEY"
+#define APP_SECRET        "APP_SECRET"
+
+#define SWITCH_ID_1       "YOUR-DEVICE-ID"    
+#define SWITCH_ID_2       "YOUR-DEVICE-ID"    
+#define SWITCH_ID_3       "YOUR-DEVICE-ID"    
+#define SWITCH_ID_4       "YOUR-DEVICE-ID"    
+
+#define BAUD_RATE         19200
+
+#define WIFI_LED       2    // WiFi LED
+
+bool connectedFlag = false;
+String pinStatus = "0000";
+String pinStatus_OLD = "0000";
+
+bool onPowerState1(const String &deviceId, bool &state) {
+  //Serial.printf("Device 1 turned %s\r\n", state?"on":"off");
+  Serial.printf("%s", state?"A1":"A0");  
+  return true; // request handled properly
 }
 
-void setup(void)
-{
-// Serial BPS debugging information via the Serial monitor
-  Serial.begin(19200);
-  pinMode(7,OUTPUT);
-  EspSerial.begin(ESP8266_BAUD);
-  delay(10);
- 
-  Blynk.begin(auth, wifi, ssid, pass);
+bool onPowerState2(const String &deviceId, bool &state) {
+  //Serial.printf("Device 2 turned %s\r\n", state?"on":"off");
+  Serial.printf("%s", state?"B1":"B0");
+  return true; // request handled properly
 }
 
-void loop(void)
-{
-  Blynk.run();
+bool onPowerState3(const String &deviceId, bool &state) {
+  //Serial.printf("Device 3 turned %s\r\n", state?"on":"off");
+  Serial.printf("%s", state?"C1":"C0");
+  return true; // request handled properly
+}
+
+bool onPowerState4(const String &deviceId, bool &state) {
+  //Serial.printf("Device 4 turned %s\r\n", state?"on":"off");
+  Serial.printf("%s", state?"D1":"D0");
+  return true; // request handled properly
+}
+
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+
+  SinricProSwitch& mySwitch2 = SinricPro[SWITCH_ID_2];
+  mySwitch2.onPowerState(onPowerState2);
+
+  SinricProSwitch& mySwitch3 = SinricPro[SWITCH_ID_3];
+  mySwitch3.onPowerState(onPowerState3);
+
+  SinricProSwitch& mySwitch4 = SinricPro[SWITCH_ID_4];
+  mySwitch4.onPowerState(onPowerState4);
+
+  // setup SinricPro
+  SinricPro.onConnected([](){ 
+    //Serial.printf("SinricPro Connected\r\n");
+    Serial.printf("W1");
+    digitalWrite(WIFI_LED, LOW);
+    }); 
+  SinricPro.onDisconnected([](){ 
+    //Serial.printf("SinricPro Disconnected\r\n");
+    Serial.printf("W0");
+    digitalWrite(WIFI_LED, HIGH);
+    });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+void updateState(String state, String pre_state){
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  SinricProSwitch& mySwitch2 = SinricPro[SWITCH_ID_2];
+  SinricProSwitch& mySwitch3 = SinricPro[SWITCH_ID_3];
+  SinricProSwitch& mySwitch4 = SinricPro[SWITCH_ID_4];
+
+  if(state.substring(0,1) != pre_state.substring(0,1)){
+    if (state.substring(0,1) == "1"){
+      mySwitch1.sendPowerStateEvent(HIGH);
+    }
+    else{
+      mySwitch1.sendPowerStateEvent(LOW);
+    }
+  }
+  if(state.substring(1,2) != pre_state.substring(1,2)){
+    if (state.substring(1,2) == "1"){
+      mySwitch2.sendPowerStateEvent(HIGH);
+    }
+    else{
+      mySwitch2.sendPowerStateEvent(LOW);
+    }
+  }
+  if(state.substring(2,3) != pre_state.substring(2,3)){
+    if (state.substring(2,3) == "1"){
+      mySwitch3.sendPowerStateEvent(HIGH);
+    }
+    else{
+      mySwitch3.sendPowerStateEvent(LOW);
+    }
+  }
+  if(state.substring(3,4) != pre_state.substring(3,4)){
+    if (state.substring(3,4) == "1"){
+      mySwitch4.sendPowerStateEvent(HIGH);
+    }
+    else{
+      mySwitch4.sendPowerStateEvent(LOW);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  Serial.printf("\r\n\r\n");
+  pinMode(WIFI_LED, OUTPUT);
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  while(Serial.available()) 
+  {
+    pinStatus = Serial.readString();
+    if(pinStatus != pinStatus_OLD) {      
+      updateState(pinStatus, pinStatus_OLD);
+      pinStatus_OLD = pinStatus;
+    }
+    //connectedFlag = true;
+  }
+  SinricPro.handle();
 }
